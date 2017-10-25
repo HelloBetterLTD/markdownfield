e6141dd723f0ba83b01e8c675d8665cc1b1ef668
Simon Erkelens <simonerkelens@silverstripe.com>
2017-10-25 19:14:17 +1300

    Minor code cleanups and removal of duplicate code or unused code.
    - Removed unneeded/unused code
    - Added docblocks to methods
    - Added use statements instead of using inline FQN
    - Ran PHP-CS-Fixer to make it PSR-2 compliant
    - Changed all arrays to shorthand (preferred syntax, and more consistent over all)

--- README.md
+++ README.md
@@ -26,9 +26,9 @@ To use the markdown DB field in your data objects the basic code would look like
 class MyDataClass extends DataObject
 {
 
-    private static $db = array(
+    private static $db = [
 		'MarkdownContent'		=> 'MarkdownText'
-	);
+	];
 
 }
 
@@ -41,9 +41,9 @@ public function getCMSFields()
 {
     $fields = parent::getCMSFields();
 
-    $fields->addFieldsToTab('Root.Sidebar', array(
+    $fields->addFieldsToTab('Root.Sidebar', [
         \SilverStripers\markdown\forms\MarkdownEditorField::create('MarkdownContent', 'Content'),
-    ));
+    ]);
 
     return $fields;
 }
--- src/forms/MarkdownEditorConfig.php
+++ src/forms/MarkdownEditorConfig.php
@@ -15,160 +15,178 @@ use SilverStripe\Core\Injector\Injectable;
 
 class MarkdownEditorConfig
 {
-
-	use Configurable;
-	use Injectable;
-
-
-	protected static $configs = array();
-	protected static $current = null;
-	private static $default_config = 'default';
-
-	protected $settings = array(
-		array(
-			'name'		=> 'heading',
-			'className'	=> 'fa fa-header',
-			'title'		=> 'Heading HTML',
-			'action'	=> 'toggleHeadingSmaller'
-		),
-		array(
-			'name'		=> 'bold',
-			'className'	=> 'fa fa-bold',
-			'title'		=> 'Bold',
-			'action'	=> 'toggleBold'
-		),
-		array(
-			'name'		=> 'italic',
-			'className'	=> 'fa fa-italic',
-			'title'		=> 'Italic',
-			'action'	=> 'toggleItalic'
-		),
-		array(
-			'name'		=> 'strikethrough',
-			'className'	=> 'fa fa-strikethrough',
-			'title'		=> 'Strike Through',
-			'action'	=> 'toggleStrikethrough'
-		),
-		"|",
-		array(
-			'name'		=> 'quote',
-			'className'	=> 'fa fa-quote-left',
-			'title'		=> 'Quote',
-			'action'	=> 'toggleBlockquote'
-		),
-		array(
-			'name'		=> 'unordered-list',
-			'action'	=> 'toggleUnorderedList',
-			'className'	=> 'fa fa-list-ul',
-            'title'		=> 'Generic List'
-		),
-		array(
-			'name'		=> 'ordered-list',
-			'action'	=> 'toggleOrderedList',
-			'className'	=> 'fa fa-list-ol',
-            'title'		=> 'Ordered List'
-		),
-		array(
-			'name'		=> 'link',
-			'action'	=> 'drawLink',
-			'className'	=> 'fa fa-link',
-            'title'		=> 'Create Link'
-		),
-		array(
-			'name'		=> 'embed',
-			'action'	=> 'ssEmbed',
-			'className'	=> 'fa fa-play',
-            'title'		=> 'Embed Media'
-		),
-		array(
-			'name'		=> 'image',
-			'action'	=> 'ssImage',
-			'className'	=> 'fa fa-picture-o',
-            'title'		=> 'Insert Image'
-		),
-		'|',
-		array(
-			'name'		=> 'preview',
-			'action'	=> 'togglePreview',
-			'className'	=> 'fa fa-eye no-disable',
-			'title'		=> 'Toggle Preview'
-		),
-		array(
-			'name'		=> 'side-by-side',
-			'action'	=> 'toggleSideBySide',
-			'className'	=> 'fa fa-columns no-disable no-mobile',
-			'title'		=> 'Toggle Side by Side'
-		),
-		array(
-			'name'		=> 'fullscreen',
-			'action'	=> 'toggleFullScreen',
-			'className'	=> 'fa fa-arrows-alt no-disable no-mobile',
-			'title'		=> 'Toggle Fullscreen'
-		),
-		'|',
-		array(
-			'name'		=> 'guide',
-			'action'	=> 'https://simplemde.com/markdown-guide',
-			'className'	=> 'fa fa-question-circle',
-			'title'		=> 'Help'
-		)
-	);
+    use Configurable;
+    use Injectable;
 
 
+    protected static $configs = [];
+    protected static $current;
+    private static $default_config = 'default';
 
-	public static function get($identifier = null)
-	{
-		if (!$identifier) {
-			return static::get_active();
-		}
-		// Create new instance if unconfigured
-		if (!isset(self::$configs[$identifier])) {
-			self::$configs[$identifier] = static::create();
-		}
-		return self::$configs[$identifier];
-	}
+    protected $settings = [
+        [
+            'name'      => 'heading',
+            'className' => 'fa fa-header',
+            'title'     => 'Heading HTML',
+            'action'    => 'toggleHeadingSmaller'
+        ],
+        [
+            'name'      => 'bold',
+            'className' => 'fa fa-bold',
+            'title'     => 'Bold',
+            'action'    => 'toggleBold'
+        ],
+        [
+            'name'      => 'italic',
+            'className' => 'fa fa-italic',
+            'title'     => 'Italic',
+            'action'    => 'toggleItalic'
+        ],
+        [
+            'name'      => 'strikethrough',
+            'className' => 'fa fa-strikethrough',
+            'title'     => 'Strike Through',
+            'action'    => 'toggleStrikethrough'
+        ],
+        "|",
+        [
+            'name'      => 'quote',
+            'className' => 'fa fa-quote-left',
+            'title'     => 'Quote',
+            'action'    => 'toggleBlockquote'
+        ],
+        [
+            'name'      => 'unordered-list',
+            'action'    => 'toggleUnorderedList',
+            'className' => 'fa fa-list-ul',
+            'title'     => 'Generic List'
+        ],
+        [
+            'name'      => 'ordered-list',
+            'action'    => 'toggleOrderedList',
+            'className' => 'fa fa-list-ol',
+            'title'     => 'Ordered List'
+        ],
+        [
+            'name'      => 'link',
+            'action'    => 'drawLink',
+            'className' => 'fa fa-link',
+            'title'     => 'Create Link'
+        ],
+        [
+            'name'      => 'embed',
+            'action'    => 'ssEmbed',
+            'className' => 'fa fa-play',
+            'title'     => 'Embed Media'
+        ],
+        [
+            'name'      => 'image',
+            'action'    => 'ssImage',
+            'className' => 'fa fa-picture-o',
+            'title'     => 'Insert Image'
+        ],
+        '|',
+        [
+            'name'      => 'preview',
+            'action'    => 'togglePreview',
+            'className' => 'fa fa-eye no-disable',
+            'title'     => 'Toggle Preview'
+        ],
+        [
+            'name'      => 'side-by-side',
+            'action'    => 'toggleSideBySide',
+            'className' => 'fa fa-columns no-disable no-mobile',
+            'title'     => 'Toggle Side by Side'
+        ],
+        [
+            'name'      => 'fullscreen',
+            'action'    => 'toggleFullScreen',
+            'className' => 'fa fa-arrows-alt no-disable no-mobile',
+            'title'     => 'Toggle Fullscreen'
+        ],
+        '|',
+        [
+            'name'      => 'guide',
+            'action'    => 'https://simplemde.com/markdown-guide',
+            'className' => 'fa fa-question-circle',
+            'title'     => 'Help'
+        ]
+    ];
 
+    /**
+     * @param null $identifier
+     * @return mixed
+     */
+    public static function get($identifier = null)
+    {
+        if (!$identifier) {
+            return static::get_active();
+        }
+        // Create new instance if unconfigured
+        if (!isset(self::$configs[$identifier])) {
+            self::$configs[$identifier] = static::create();
+        }
 
-	public static function get_active_identifier()
-	{
-		$identifier = self::$current ?: static::config()->get('default_config');
-		return $identifier;
-	}
+        return self::$configs[$identifier];
+    }
 
-	public static function get_active()
-	{
-		$identifier = self::get_active_identifier();
-		return self::get($identifier);
-	}
+    /**
+     * @return mixed
+     */
+    public static function get_active_identifier()
+    {
+        return static::$current ?: static::config()->get('default_config');
+    }
 
-	public static function set_active(MarkdownEditorConfigs $config)
-	{
-		$identifier = static::get_active_identifier();
-		return static::set_config($identifier, $config);
-	}
+    /**
+     * @return mixed
+     */
+    public static function get_active()
+    {
+        return static::get_active_identifier();
+    }
 
-	public static function set_config($identifier, MarkdownEditorConfigs $config = null)
-	{
-		if ($config) {
-			self::$configs[$identifier] = $config;
-		} else {
-			unset(self::$configs[$identifier]);
-		}
-		return $config;
-	}
+    /**
+     * @param MarkdownEditorConfig $config
+     * @return MarkdownEditorConfig
+     */
+    public static function set_active(MarkdownEditorConfig $config)
+    {
+        return static::get_active_identifier();
+    }
 
-	public function getConfig()
-	{
-		return $this->settings;
-	}
+    /**
+     * @param $identifier
+     * @param MarkdownEditorConfig|null $config
+     * @return MarkdownEditorConfig
+     */
+    public static function set_config($identifier, MarkdownEditorConfig $config = null)
+    {
+        if ($config) {
+            static::$configs[$identifier] = $config;
+        } else {
+            unset(static::$configs[$identifier]);
+        }
 
-	public function getAttributes()
-	{
-		return [
-			'data-editor' => 'markDown',
-			'data-config' => Convert::array2json($this->getConfig()),
-		];
-	}
+        return $config;
+    }
 
+    /**
+     * @return array
+     */
+    public function getConfig()
+    {
+        return $this->settings;
+    }
 
+    /**
+     * @return array
+     */
+    public function getAttributes()
+    {
+        return [
+            'data-editor' => 'markDown',
+            'data-config' => Convert::array2json($this->getConfig()),
+        ];
+    }
 }
